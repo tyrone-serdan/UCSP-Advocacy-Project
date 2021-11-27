3a42e56a11b79f6b999ddd2f1e576904824779f1
Tyrone Serdan <tyroneb.serdan@gmail.com>
2021-11-28 01:15:28 +0800

    started designing site-main

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let bool = new Boolean();
 
+// I'll plan on doing something to the bool variable later.
 function warnUser() {
     bool = confirm("WARNING! THIS WEBSITE IS A SCHOOL PROJECT AND NOT A GENUINE ADVOCACY CAMPAIGN. PLEASE PRESS OK IF YOU HAVE READ THIS MESSAGE.");
 }
 
-setInterval(warnUser(), 3000);
+warnUser();
